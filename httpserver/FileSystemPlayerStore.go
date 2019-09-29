63dfc0d313f7a59a12f02c2ebafb019b511c4e3c
fox <jiaqing.li@lixil.com>
2019-09-29 11:18:58 +0800

    code refactor

--- httpserver/FileSystemPlayerStore.go
+++ httpserver/FileSystemPlayerStore.go
@@ -1,7 +1,6 @@
 package httpserver
 
 import (
-	"encoding/json"
 	"io"
 )
 
@@ -18,7 +17,6 @@ func (f FileSystemPlayerStore) RecordWin(name string) {
 }
 
 func (f *FileSystemPlayerStore) GetLeague() []Player {
-	var league []Player
-	json.NewDecoder(f.database).Decode(&league)
+	league, _ := NewLeague(f.database)
 	return league
 }
